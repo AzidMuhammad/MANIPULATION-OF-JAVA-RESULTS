0e95bbeec9b2e63b181bd7cbdba4f876e4d0dd53
ZIDD <156231466+AzidMuhammad@users.noreply.github.com>
2024-02-25 23:59:13 +0700

    Add files via upload

---
+++ PemilihanKetuaKelas.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class PemilihanKetuaKelas {
+    public static void main(String[] args) {
+        ArrayList<String> kandidat = new ArrayList<>();
+        kandidat.add("Andi");
+        kandidat.add("Budi");
+        kandidat.add("Cindy");
+        kandidat.add("Dedi");
+
+        String ketuaKelas = "Budi";
+
+        System.out.println("Ketua kelas yang terpilih adalah: " + ketuaKelas);
+    }
+}
